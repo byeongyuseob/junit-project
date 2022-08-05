d16272bb8dc06fdcb2131e18dc60fd55358aa046
Byeon gyuseob <byeongyuseob@kakao.com>
2022-08-06 02:37:56 +0900

    set repository test env

--- src/main/java/com/example/junitproject/domain/BookRepository.java
+++ src/main/java/com/example/junitproject/domain/BookRepository.java
@@ -1,5 +1,9 @@
 package com.example.junitproject.domain;
 
-public class BookRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
 
 }
--- src/test/java/com/example/junitproject/domain/BookRepositoryTest.java
+++ src/test/java/com/example/junitproject/domain/BookRepositoryTest.java
@@ -1,7 +1,29 @@
 package com.example.junitproject.domain;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest // DB와 관련된 컴포넌트들만 메모리에 로딩 (Unit test env)1
 class BookRepositoryTest {
+
+    @Autowired // DI
+    private BookRepository bookRepository;
+
+    // 1. 책 등록
+    @Test
+    public void 책등록_test(){
+        System.out.println("책등록 테스트 실행");
+    }
+
+    // 2. 책 목록보기
+
+    // 3. 책 한 권 보기
+
+    // 4. 책 수정
+
+    // 5. 책 삭제
 
 }
